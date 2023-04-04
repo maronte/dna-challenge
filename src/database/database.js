d9e4c8217a5ecf8f765eb05182029fc40aff4af7
Ronaldo Trejo <ma.ronaldotrejo@gmail.com>
2023-04-04 03:54:04 -0600

    feat: add database and ORM configuration

---
+++ src/database/database.js
@@ -0,0 +1,39 @@
+const { Sequelize } = require('sequelize')
+const { variables } = require('../config/config')
+
+const sequelize = new Sequelize({
+  dialect: 'mysql',
+  database: variables.database.name,
+  port: variables.database.port,
+  username: variables.database.user,
+  host: variables.database.host,
+  pool: {
+    max: 1,
+    min: 1,
+    /*
+     * Set this value to 0 so connections are eligible for cleanup immediately after they're
+     * returned to the pool.
+     */
+    idle: 0,
+    // Choose a small enough value that fails fast if a connection takes too long to be established.
+    acquire: 30000,
+    /*
+     * Ensures the connection pool attempts to be cleaned up automatically on the next Lambda
+     * function invocation, if the previous invocation timed out.
+     */
+    evict: variables.lambda.timeout
+  },
+  define: {
+    timestamps: false
+  }
+})
+
+async function loadSequelize () {
+  await sequelize.authenticate()
+  return sequelize
+}
+
+module.exports = {
+  sequelize,
+  loadSequelize
+}
